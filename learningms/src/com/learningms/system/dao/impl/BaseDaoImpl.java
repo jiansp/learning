e1ea238b87fc9bb84a5cd184be1eb84b2012cd0c
jiansp <kkjane@163.com>
2017-09-06 14:36:37 +0800

    finish baseDao

--- learningms/src/com/learningms/system/dao/impl/BaseDaoImpl.java
+++ learningms/src/com/learningms/system/dao/impl/BaseDaoImpl.java
@@ -20,28 +20,128 @@ public class BaseDaoImpl implements BaseDao{
 	public Session getCurrentSession(){
 		return sessionFactory.getCurrentSession();
 	}
-	
+	/*
+	 * 保存
+	 */
 	public Serializable save(Object object){
 		Serializable id = getCurrentSession().save(object);
 		return id;
 	}
-	
+	/*
+	 * 删除
+	 */
 	public void delete(Object object){
 		getCurrentSession().delete(object);
 	}
-	
+	/*
+	 * 更新
+	 */
 	public void update(Object object){
 		getCurrentSession().update(object);
 	}
-	
+	/*
+	 * 通过id查找
+	 */
 	public Object findById(Class clazz, Serializable id){
 		return getCurrentSession().get(clazz, id);
 	}
-	
+	/*
+	 * 不带参数的hql语句查询
+	 */
 	public List findByHql(String hql){
 		Session session = getCurrentSession();
 		Query query = session.createQuery(hql);
 		List ret = query.list();
 		return ret;
 	}
+	/*
+	 * 不带参数的sql语句查询
+	 */
+	public List findBySql(String Sql){
+		Session session = getCurrentSession();
+		Query query = session.createSQLQuery(Sql);
+		List ret = query.list();
+		return ret;
+	}
+	/*
+	 * 不带参数名的hql语句查询
+	 */
+	public List findByValueHql(String hql, Object[] paramValue){
+		Session session = getCurrentSession();
+		Query query = session.createQuery(hql);
+		if(paramValue != null){
+			for(int i=0; i<paramValue.length;i++){
+				query.setParameter(i, paramValue[i]);
+			}
+		}
+		List ret = query.list();
+		return ret;
+	}
+	/*
+	 * 不带参数名的sql语句查询
+	 */
+	public List findByValueSql(String sql, Object[] paramValue){
+		Session session = getCurrentSession();
+		Query query = session.createSQLQuery(sql);
+		if(paramValue != null){
+			for(int i=0; i<paramValue.length;i++){
+				query.setParameter(i, paramValue[i]);
+			}
+		}
+		List ret = query.list();
+		return ret;
+	}
+	/*
+	 * 带参数名的hql语句查询
+	 */
+	public List findByNameHql(String hql,String[] paramName, Object[] paramValue){
+		Session session = getCurrentSession();
+		Query query = session.createQuery(hql);
+		if(paramValue != null){
+			for(int i=0; i<paramValue.length;i++){
+				query.setParameter(paramName[i], paramValue[i]);
+			}
+		}
+		List ret = query.list();
+		return ret;
+	}
+	/*
+	 * 带参数名的sql语句查询
+	 */
+	public List findByNameSql(String sql,String[] paramName, Object[] paramValue){
+		Session session = getCurrentSession();
+		Query query = session.createSQLQuery(sql);
+		if(paramValue != null){
+			for(int i=0; i<paramValue.length;i++){
+				query.setParameter(paramName[i], paramValue[i]);
+			}
+		}
+		List ret = query.list();
+		return ret;
+	}
+	/*
+	 * 一个参数名的hql语句in查询
+	 */
+	public List findByNameHqlForIn(String hql,String paramName, Object[] paramValue){
+		Session session = getCurrentSession();
+		Query query = session.createQuery(hql);
+		if(paramValue != null){
+			query.setParameterList(paramName, paramValue);
+		}
+		List ret = query.list();
+		return ret;
+	}
+	/*
+	 * 一个参数名的sql语句in查询
+	 */
+	public List findByNameSqlForIn(String sql,String paramName, Object[] paramValue){
+		Session session = getCurrentSession();
+		Query query = session.createSQLQuery(sql);
+		if(paramValue != null){
+			query.setParameterList(paramName, paramValue);
+		}
+		List ret = query.list();
+		return ret;
+	}
+	
 }
